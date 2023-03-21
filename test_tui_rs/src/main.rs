7024eee1526b0bbddf47e6c8876b29b2c605cf68
abhay <abhaysp95@protonmail.com>
2023-03-21 22:44:47 +0530

    feat: tui-rs demo, initial commit

---
+++ test_tui_rs/src/main.rs
@@ -0,0 +1,307 @@
+use std::{
+    fs, io,
+    sync::mpsc,
+    thread,
+    time::{Duration, Instant},
+};
+
+use chrono::{DateTime, Utc};
+use crossterm::{
+    event::{self, Event as CEvent, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+use tui::{
+    backend::CrosstermBackend,
+    layout::{Alignment, Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans},
+    widgets::{
+        Block, BorderType, Borders, Cell, List, ListItem, ListState, Paragraph, Row, Table, Tabs,
+    },
+    Terminal,
+};
+
+const DB_PATH: &str = "./data/db.json";
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Pet {
+    id: usize,
+    name: String,
+    category: String,
+    age: usize,
+    created_at: DateTime<Utc>,
+}
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
+}
+
+enum Event<I> {
+    Input(I),
+    Tick,
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+enum MenuItem {
+    Home,
+    Pets,
+}
+
+impl From<MenuItem> for usize {
+    fn from(value: MenuItem) -> Self {
+        match value {
+            MenuItem::Home => 0,
+            MenuItem::Pets => 1,
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    enable_raw_mode().expect("can't run in raw mode");
+
+    let (tx, rx) = mpsc::channel();
+    let tick_rate = Duration::from_millis(200);
+    thread::spawn(move || {
+        let mut last_tick = Instant::now();
+        loop {
+            let timeout = tick_rate
+                .checked_sub(last_tick.elapsed())
+                .unwrap_or_else(|| Duration::from_secs(0));
+
+            if event::poll(timeout).expect("poll works") {
+                if let CEvent::Key(key) = event::read().expect("can read events") {
+                    tx.send(Event::Input(key)).expect("can send events");
+                }
+            }
+
+            if last_tick.elapsed() >= tick_rate {
+                if let Ok(_) = tx.send(Event::Tick) {
+                    last_tick = Instant::now();
+                }
+            }
+        }
+    });
+
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+
+    let menu_titles = vec!["Home", "Pets", "Add", "Delete", "Quit"];
+    let mut active_menu_item = MenuItem::Home;
+
+    let mut pet_list_state = ListState::default();
+    pet_list_state.select(Some(0));
+
+    #[allow(unreachable_code)]
+    loop {
+        terminal.draw(|rect| {
+            let size = rect.size();
+            let chunks = Layout::default()
+                .direction(Direction::Vertical)
+                .margin(2)
+                .constraints(
+                    [
+                        Constraint::Length(3),
+                        Constraint::Min(2),
+                        Constraint::Length(3),
+                    ]
+                    .as_ref(),
+                )
+                .split(size);
+
+            let copyright = Paragraph::new("pet-CLI-2023 - no rights reserverd")
+                .style(Style::default().fg(Color::LightCyan))
+                .alignment(Alignment::Center)
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(Color::White))
+                        .title("Copyright")
+                        .border_type(BorderType::Plain),
+                );
+
+            let menu = menu_titles
+                .iter()
+                .map(|tab| {
+                    let (first, rest) = tab.split_at(1);
+                    Spans::from(vec![
+                        Span::styled(
+                            first,
+                            Style::default()
+                                .fg(Color::Yellow)
+                                .add_modifier(Modifier::UNDERLINED),
+                        ),
+                        Span::styled(rest, Style::default().fg(Color::White)),
+                    ])
+                })
+                .collect::<Vec<Spans>>();
+
+            let tabs = Tabs::new(menu)
+                .select(active_menu_item.into())
+                .block(Block::default().title("Menu").borders(Borders::ALL))
+                .style(Style::default().fg(Color::White))
+                .highlight_style(Style::default().fg(Color::Yellow))
+                .divider(Span::raw("|"));
+
+            /* let body = Paragraph::new("This is just a paragraph")
+            .style(Style::default().fg(Color::Green))
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .title("Paragraph")
+                    .style(Style::default().fg(Color::White)),
+            ); */
+
+            rect.render_widget(tabs, chunks[0]);
+            rect.render_widget(copyright, chunks[2]);
+
+            match active_menu_item {
+                MenuItem::Home => rect.render_widget(render_home(), chunks[1]),
+                MenuItem::Pets => {
+                    let pet_chunks = Layout::default()
+                        .direction(Direction::Horizontal)
+                        .constraints(
+                            [Constraint::Percentage(20), Constraint::Percentage(80)].as_ref(),
+                        )
+                        .split(chunks[1]);
+                    let (left, right) = render_pets(&pet_list_state);
+                    rect.render_stateful_widget(left, pet_chunks[0], &mut pet_list_state);
+                    rect.render_widget(right, pet_chunks[1]);
+                }
+            }
+        })?;
+
+        match rx.recv()? {
+            Event::Input(event) => match event.code {
+                KeyCode::Char('q') => {
+                    disable_raw_mode()?;
+                    terminal.show_cursor()?;
+                    break;
+                }
+                KeyCode::Char('h') => active_menu_item = MenuItem::Home,
+                KeyCode::Char('p') => active_menu_item = MenuItem::Pets,
+                _ => {}
+            },
+            Event::Tick => {}
+        }
+    }
+
+    Ok(())
+}
+
+fn render_home<'p>() -> Paragraph<'p> {
+    let home = Paragraph::new(vec![
+                              Spans::from(vec![Span::raw("")]),
+                              Spans::from(vec![Span::raw("Welcome")]),
+                              Spans::from(vec![Span::raw("")]),
+                              Spans::from(vec![Span::raw("to")]),
+                              Spans::from(vec![Span::raw("")]),
+                              Spans::from(vec![Span::raw("")]),
+                              Spans::from(vec![Span::styled("pet-CLI", Style::default().fg(Color::LightBlue))]),
+                              Spans::from(vec![Span::raw("")]),
+                              Spans::from(vec![Span::raw(
+                                      "Press 'p' to access pets, 'a' to add random new pets and 'd' to delete currently selected pet."
+                                      )]), ])
+        .alignment(Alignment::Center)
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .style(Style::default().fg(Color::White))
+                .title("Home").border_type(BorderType::Plain));
+
+    home
+}
+
+fn read_db() -> Result<Vec<Pet>, Error> {
+    let db_content = fs::read_to_string(DB_PATH)?;
+
+    Ok(serde_json::from_str(&db_content)?)
+}
+
+fn render_pets<'a>(pet_list_state: &ListState) -> (List<'a>, Table<'a>) {
+    let pets = Block::default()
+        .borders(Borders::ALL)
+        .style(Style::default().fg(Color::White))
+        .title("Pets")
+        .border_type(BorderType::Plain);
+
+    let pet_list = read_db().expect("can fetch pet list");
+
+    let items = pet_list
+        .iter()
+        .map(|pet| {
+            ListItem::new(Spans::from(vec![Span::styled(
+                pet.name.clone(),
+                Style::default(),
+            )]))
+        })
+        .collect::<Vec<_>>();
+
+    let selected_pet = pet_list
+        .get(
+            pet_list_state
+                .selected()
+                .expect("there is always a selected pet"),
+        )
+        .expect("exists"); // remember to clone
+
+    let list = List::new(items).block(pets).highlight_style(
+        Style::default()
+            .bg(Color::Yellow)
+            .fg(Color::Black)
+            .add_modifier(Modifier::BOLD),
+    );
+
+    let pet_detail = Table::new(vec![Row::new(vec![
+        Cell::from(Span::raw(selected_pet.id.to_string())),
+        Cell::from(Span::raw(selected_pet.name.to_string())),
+        Cell::from(Span::raw(selected_pet.category.to_string())),
+        Cell::from(Span::raw(selected_pet.age.to_string())),
+        Cell::from(Span::raw(selected_pet.created_at.to_string())),
+    ])])
+    .header(Row::new(vec![
+        Cell::from(Span::styled(
+            "ID",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+        Cell::from(Span::styled(
+            "Name",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+        Cell::from(Span::styled(
+            "Category",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+        Cell::from(Span::styled(
+            "Age",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+        Cell::from(Span::styled(
+            "Created At",
+            Style::default().add_modifier(Modifier::BOLD),
+        )),
+    ]))
+    .block(
+        Block::default()
+            .borders(Borders::ALL)
+            .style(Style::default().fg(Color::White))
+            .title("Detail")
+            .border_type(BorderType::Plain),
+    )
+    .widths(&[
+        Constraint::Percentage(5),
+        Constraint::Percentage(20),
+        Constraint::Percentage(20),
+        Constraint::Percentage(5),
+        Constraint::Percentage(20),
+    ]);
+
+    (list, pet_detail)
+}
